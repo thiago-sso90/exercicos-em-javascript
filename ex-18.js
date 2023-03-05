d985716cdd4ae641f623dfb25190fc4fbb736127
THIAGO DE SOUZA BARBOSA <thiaguinhosouza73@gmail.com>
2023-03-05 14:27:33 -0300

    corregindo ex 18

--- ex-18.js
+++ ex-18.js
@@ -1,27 +1,28 @@
 //18. Escrever um algoritmo que lê um vetor K(15). Crie, a seguir, um vetor P, que contenha todos os números primos de
 //K. Escreva o vetor P.
 
-var k = 15;
-
-function numerosprimos(k){
-    
-    var numprimos = [];
-
-    for(var i = 0; i < k; i++){
-        var divisores = 0;
-
-        for( var b = 0; i < b; b++){
-            if(i % i == 0){
-                divisores++
-            }
-           
-        }
-        
-        if(divisores % i == 0){
-          numprimos.push(i)
-        }
-    }
-  return numprimos
-
+let list4 = [];
+let listP = [];
+let numPrimo = 0;
+for (let i = 0; i < 20; i++) {
+   list4.push(i);
 }
+let numDivisores = 0;
+
+for (numPrimo = 0; numPrimo <= list4.length; numPrimo++) {
+
+   for (let i = 1; i <= numPrimo; i++) {
+
+       if (numPrimo % i == 0) {
+
+           numDivisores++;
+       }
+   }
+   if (numDivisores == 2) {
+
+       listP.push(numPrimo);
+   }
+   numDivisores = 0;
+}
+console.log(listP);
 
